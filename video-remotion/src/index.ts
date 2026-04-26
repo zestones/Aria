d19b96327159e88ac8ea84b4723066df1c11502b
zestones <idrissbenguezzou@gmail.com>
2026-04-26 20:29:40 +0200

    refactor: remove remotion project from the repos

--- video-remotion/src/index.ts
+++
@@ -1,5 +0,0 @@
-// Entry point for Remotion. See https://remotion.dev/docs/register-root
-import { registerRoot } from "remotion";
-import { RemotionRoot } from "./Root";
-
-registerRoot(RemotionRoot);
